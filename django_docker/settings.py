bc57927281fd11ce3a02834b5e23c6fb355a2869
donaisore <r.h.kobe0706@gmail.com>
2020-02-13 09:14:58 +0900

    rename app from blogs to vlogs.

--- django_docker/settings.py
+++ django_docker/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blogs.apps.BlogsConfig',
+    'vlogs.apps.VlogsConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
